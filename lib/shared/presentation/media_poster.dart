5f45639ab3bc8d1fe5d38f93db179e03dab4e321
Mahmoud Abulmagd <me@abulmagd.dev>
2023-07-03 16:08:13 +0300

    - change arch

--- lib/shared/presentation/media_poster.dart
+++ lib/shared/presentation/media_poster.dart
@@ -1,52 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:movie_flix/shared/data/environment_variables.dart';
-
+import 'package:movie_flix/shared/presentation/frosted_container.dart';
 import '../../config/theme/palette.dart';
 import '../domain/movie.dart';
 
-class MoviePoster extends StatelessWidget {
+class MoviePoster extends StatefulWidget {
   const MoviePoster({Key? key, required this.movie}) : super(key: key);
 
   final Movie movie;
 
   @override
+  State<MoviePoster> createState() => _MoviePosterState();
+}
+
+class _MoviePosterState extends State<MoviePoster> {
+  bool showInfo = false;
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return AspectRatio(
-      aspectRatio: 9 / 16,
+      aspectRatio: 2 / 3,
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(4),
-        child: Column(
-          children: [
-            Expanded(
-              child: Image.network(
-                '${RemoteEnvironment.tmdbImage}${movie.posterPath}',
+        borderRadius: BorderRadius.circular(12),
+        child: GestureDetector(
+          onTap: () => setState(() {
+            showInfo = !showInfo;
+          }),
+          child: Stack(
+            children: [
+              Image.network(
+                '${RemoteEnvironment.tmdbImage}${widget.movie.posterPath}',
+                width: double.infinity,
                 fit: BoxFit.cover,
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(4),
-              color: Colors.blueGrey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(movie.title, maxLines: 1, overflow: TextOverflow.ellipsis),
-                  Row(
+              AnimatedOpacity(
+                opacity: showInfo ? 1 : 0,
+                duration: const Duration(milliseconds: 200),
+                child: FrostedContainer(
+                  tightPadding: true,
+                  borderRadius: 0,
+                  blurStrength: 8,
+                  child: Column(
                     children: [
-                      Text('${movie.releaseDate.year}'),
+                      Text(widget.movie.title, maxLines: 2),
+                      Wrap(),
                       const Spacer(),
-                      const Icon(
-                        Icons.star_rounded,
-                        color: Palette.starOrange,
-                        size: 14,
+                      Row(
+                        children: [
+                          Text('${widget.movie.releaseDate.year}'),
+                          const Spacer(),
+                          const Icon(
+                            Icons.star_rate_rounded,
+                            color: Palette.starOrange,
+                            size: 20,
+                          ),
+                          Text('${widget.movie.voteAverage}'),
+                        ],
                       ),
-                      const SizedBox(width: 2),
-                      Text(movie.voteAverage.toString()),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: theme.elevatedButtonTheme.style?.copyWith(
+                          minimumSize: MaterialStateProperty.all(
+                            const Size.fromHeight(32),
+                          ),
+                          padding: MaterialStateProperty.all(EdgeInsets.zero),
+                          shape: MaterialStateProperty.all(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.zero),
+                            ),
+                          ),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        ),
+                        child: const Text('Explore'),
+                      ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
